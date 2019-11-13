7d7027194c31f06c97d89981700ccef5ac4d2a33
jaredbucko <jaredbucko@icloud.com>
2019-11-12 20:19:05 -0800

    deleted external css file

--- questions.js
+++ questions.js
@@ -1,3 +1,11 @@
+// variables
+var highscores = document.querySelector("#highscores");
+var countdown = document.querySelector("#countdown");
+var title = document.querySelector("#title");
+var welcome = document.querySelector("#welcome");
+var startbtn = document.querySelector("#startbtn");
+
+// questions
 var questions = [
   {
     title: "Commonly used data types DO NOT include:",
@@ -9,5 +17,40 @@ var questions = [
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses"
   },
-  ///etc.
 ];
+
+// home page
+countdown.innerHTML = (questions.length*15);
+
+// start game
+startbtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  setTime();
+  askQuestions();
+});
+
+var secondsLeft = (questions.length*15)
+
+function setTime () {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    countdown.innerHTML = secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      return;
+    }
+
+  }, 1000);
+}
+
+function askQuestions () {
+  welcome.remove();
+  startbtn.remove();
+
+  for (var i = 0; i < questions.length; i++) {
+
+
+  }
+
+}
